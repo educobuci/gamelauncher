d1075e36a00e176e2f036b5ff1dae433defb0ad2
Eduardo Cobuci <educobuci@gmail.com>
2017-07-10 00:12:16 -0300

    Focus nav prototype

--- javascript/renderer.js
+++ javascript/renderer.js
@@ -3,7 +3,7 @@
 // All of the Node.js APIs are available in this process.
 
 var render = (index) => document.write(
-  '<div tabindex="'+ index + '" class="item">' + index + '<img width="300"  src="http://cdn.wccftech.com/wp-content/uploads/2015/06/Dark-Souls-III-19.jpg"></div>'
+  '<div id=' + index + ' tabindex="' + index + '" class="item">' + index + '<img width="300"  src="http://cdn.wccftech.com/wp-content/uploads/2015/06/Dark-Souls-III-19.jpg"></div>'
 )
 var renderList = (index) => {
   document.write('<h4>PC</h4><div class="list">')
@@ -15,3 +15,19 @@ var renderList = (index) => {
 for (var i = 0; i < 10; i++) {
   renderList(i)
 }
+
+$(document).keydown(function (e) {
+  var $current = $(":focus");
+  if (e.which === 37) {
+    $current.prev().focus();
+  } else if (e.which === 39) {
+    $current.next().focus();
+  }
+  // var index = $(":focus").index() + 1;
+  // console.log(index);
+  // if (e.which === 37) {
+  //   $('div :nth-child(' + (index - 1) + ')').focus();
+  // } else if (e.which === 39) {
+  //   $('div :nth-child(' + (index + 1) + ')').focus();
+  // }
+});
