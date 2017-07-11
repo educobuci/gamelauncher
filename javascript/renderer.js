5e1ea71a6062a4dae6387b2461c13595376068a6
Eduardo Cobuci <educobuci@gmail.com>
2017-07-10 23:05:42 -0300

    Improve vertical scrolling

--- javascript/renderer.js
+++ javascript/renderer.js
@@ -1,9 +1,11 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const VISIBILITY_MARGIN = 50;
+const ScrollDirection = {
+  UP: Symbol("up"),
+  DOWN: Symbol("down")
+}
 
 var render = (index) => document.write(
-  '<div id=' + index + ' tabindex="' + index + '" class="item">' + index + '<img width="300"  src="http://cdn.wccftech.com/wp-content/uploads/2015/06/Dark-Souls-III-19.jpg"></div>'
+  '<div id=' + index + ' tabindex="' + index + '" class="item">' + index + '<img width="200"  src="http://cdn.wccftech.com/wp-content/uploads/2015/06/Dark-Souls-III-19.jpg"></div>'
 )
 var renderList = (index) => {
   document.write('<h4>PC</h4><div class="list">')
@@ -23,13 +25,13 @@ $(document).keydown(function (e) {
       $current.prev().focus();
       break;
     case 38:
-      jFocus(e, $current.parent().prev().prev().children().first());
+      jFocus(e, $current.parent().prev().prev().children().first(), ScrollDirection.UP);
       break;
     case 39:
       $current.next().focus();
       break;
     case 40:
-      jFocus(e, $current.parent().next().next().children().first());
+      jFocus(e, $current.parent().next().next().children().first(), ScrollDirection.DOWN);
       break;
     default:
       break;
@@ -45,28 +47,44 @@ var cursorFocus = function (elem) {
 
 var anim = null;
 
-var jFocus = function (e, $target) {
-  if(!$target.parent().prev().offset()) {
+var jFocus = function (e, $target, direction) {
+  if (!$target.parent().prev().offset()) {
     return;
   }
   e.preventDefault();
-  if (anim === null) {
-    anim = $('html, body').animate({
-      scrollTop: $target.parent().prev().offset().top
-    }, 160,  "swing", function () {
-      anim = null;
-      // Callback after animation
-      // Must change focus!
-      $target.focus();
-      if ($target.is(":focus")) { // Checking if the target was focused
-        return false;
-      } else {
-        console.warn("it should'n happen")
-        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-        $target.focus(); // Set focus again
-      };
-    });
+  if (!anim) {
+    var top = 0;
+    var shouldScroll = false;
+    if (direction == ScrollDirection.DOWN) {
+      top = Math.floor(($target.outerHeight() + $target.offset().top) - $(window).height()) +
+        VISIBILITY_MARGIN
+      shouldScroll = ($(window).height() + window.scrollY) < ($target.outerHeight() + $target.offset().top)
+    } else {
+      top = $target.offset().top - VISIBILITY_MARGIN
+      shouldScroll = $target.offset().top < window.scrollY
+    }
+    if (shouldScroll) {
+      anim = $('html, body').animate({
+        scrollTop: top
+      }, 160, "swing", function () {
+        anim = null;
+        return eFocus($target)
+      });
+    } else {
+      eFocus($target)
+    }
   } else {
     window.scrollTo(0, $target.parent().prev().offset().top);
   }
+}
+
+var eFocus = ($target) => {
+  $target.focus();
+  if ($target.is(":focus")) { // Checking if the target was focused
+    return false;
+  } else {
+    console.warn("it should'n happen")
+    $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+    $target.focus(); // Set focus again
+  };
 }
